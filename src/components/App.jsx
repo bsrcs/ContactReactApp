29acc82642cc68a4886a2feaa1c3bdc8cdd836e5
bsrcs <bsr.ercelik@gmail.com>
2020-07-24 12:05:44 +0300

    Map data to the components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,30 +3,37 @@ import contacts from "../contacts";
 import Card from "./Card";
 import Avatar from "./Avatar";
 
+// when this function called it gets passed over a single contact
+// such as contact[0]
+function createCard(contact) {
+  //return a custom card component
+  // get hold of the properties
+  // key property must be unique across all the repeated components
+  return( 
+    <Card 
+      id={contact.id}
+      key= {contact.id}
+      name={contact.name}
+      img={contact.imageURL}
+      phone={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
 
       <Avatar img="https://image.flaticon.com/icons/png/512/53/53094.png" />
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].imageURL}
-        phone={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imageURL}
-        phone={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imageURL}
-        phone={contacts[2].phone}
-        email={contacts[2].email}
-      />
+
+      {/* 
+       tap into "contacts" array & pass entire "createCard" func into map func
+       map func loops through the array "contacts" & for every single item that
+       exists in the array, it calls "createCard" func & pass over each of the objects
+      */} 
+      {contacts.map(createCard)}
     </div>
   );
 }
